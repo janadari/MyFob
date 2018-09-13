ec6479bd23fbbf3bc4f622303bf4e021ada0f25e
shehani <janadari1995@gmail.com>
2018-09-13 11:21:58 +0530

    categories for The Fob

---
+++ app/src/main/java/shehani/thefobnew/ShoppingCartOption.java
@@ -0,0 +1,56 @@
+package shehani.thefobnew;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+public class ShoppingCartOption extends AppCompatActivity {
+    ListView list;
+    String[] itemname = {
+            "ebay","aliExpress"
+    };
+
+    Integer[] imgid = {
+            R.drawable.ebay, R.drawable.aliexpress
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_cart_option);
+
+        CustomListAdapter adapter = new CustomListAdapter(this, itemname, imgid);
+        list = (ListView) findViewById(R.id.list_shopping_cart);
+        list.setAdapter(adapter);
+
+    }
+    public boolean onCreateOptionMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.option_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+
+    }
+}
